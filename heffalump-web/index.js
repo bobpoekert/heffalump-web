2b898735a55f1c76a530451f6bded85ad41404bb
Bob Poekert <bob@poekert.com>
2017-04-03 08:48:23 +0000

    remove old index.js

--- heffalump-web/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Mastodon} from './javascripts/components/containers/mastodon';
-
-ReactDOM.render(
-    (<Mastodon />),
-    document.getElementById('app-holder')
-);
